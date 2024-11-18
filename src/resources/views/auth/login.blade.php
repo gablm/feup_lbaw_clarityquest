6ad352d4db78b3e5649ef1c68a91025493c11284
Gabriel Lima <44784408+gablm@users.noreply.github.com>
2024-11-18 15:19:41 +0000

    login page

--- src/resources/views/auth/login.blade.php
+++ src/resources/views/auth/login.blade.php
@@ -1,37 +1,49 @@
 @extends('layouts.auth')
 
 @section('content')
-<form method="POST" action="{{ route('login') }}">
-    {{ csrf_field() }}
+<div class="min-w-[35vw]">
+	<form method="POST" action="{{ route('login') }}">
+		{{ csrf_field() }}
 
-    <label for="email">E-mail</label>
-    <input id="email" type="email" name="email" value="{{ old('email') }}" required autofocus>
-    @if ($errors->has('email'))
-        <span class="error">
-          {{ $errors->first('email') }}
-        </span>
-    @endif
+		<div class="mb-4">
+			<label class="block text-gray-700 text-sm font-bold mb-2" for="email">E-mail</label>
+			<input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="email" name="email" value="{{ old('email') }}" required>
+			@if ($errors->has('email'))
+			<span class="text-red-500 text-xs bold">
+				{{ $errors->first('email') }}
+			</span>
+			@endif
+		</div>
 
-    <label for="password" >Password</label>
-    <input id="password" type="password" name="password" required>
-    @if ($errors->has('password'))
-        <span class="error">
-            {{ $errors->first('password') }}
-        </span>
-    @endif
+		<div class="mb-4">
+			<label class="block text-gray-700 text-sm font-bold mb-2" for="password">Password</label>
+			<input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" name="password" required>
+			@if ($errors->has('password'))
+			<span class="text-red-500 text-xs bold">
+				{{ $errors->first('password') }}
+			</span>
+			@endif
+		</div>
 
-    <label>
-        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
-    </label>
+		<div class="mb-6">
+			<label class="appearance-none rounded text-gray-700 leading-tight focus:outline-none">
+				<input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
+			</label>
+		</div>
 
-    <button class="button button-outline" type="submit">
-        Login
-    </button>
-    <a class="button button-outline" href="{{ route('register') }}">Register</a>
-    @if (session('success'))
-        <p class="success">
-            {{ session('success') }}
-        </p>
-    @endif
-</form>
+		<div class="flex items-center justify-between">
+			<button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+				Sign In
+			</button>
+			<a class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="{{ route('register') }}">
+				Register
+			</a>
+		</div>
+		@if (session('success'))
+		<p class="success">
+			{{ session('success') }}
+		</p>
+		@endif
+	</form>
+</div>
 @endsection
--- src/resources/views/layouts/auth.blade.php
+++ src/resources/views/layouts/auth.blade.php
@@ -1,38 +1,37 @@
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- CSRF Token -->
-        <meta name="csrf-token" content="{{ csrf_token() }}">
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Styles -->
-        <link href="{{ url('css/milligram.min.css') }}" rel="stylesheet">
-		<link href="{{ url('css/app.css') }}" rel="stylesheet">
-		
-		@vite('resources/css/app.css')
-		
-        <script type="text/javascript">
-            // Fix for Firefox autofocus CSS bug
-            // See: http://stackoverflow.com/questions/18943276/html-5-autofocus-messes-up-css-loading/18945951#18945951
-        </script>
-        <script type="text/javascript" src={{ url('js/app.js') }} defer>
-        </script>
-    </head>
-    <body>
-        <main class="flex flex-col items-center h-screen">
+	<!-- CSRF Token -->
+	<meta name="csrf-token" content="{{ csrf_token() }}">
+	<title>{{ config('app.name', 'Laravel') }}</title>
+
+	<!-- Styles -->
+	@vite('resources/css/app.css')
+
+	<script type="text/javascript">
+		// Fix for Firefox autofocus CSS bug
+		// See: http://stackoverflow.com/questions/18943276/html-5-autofocus-messes-up-css-loading/18945951#18945951
+	</script>
+	<script type="text/javascript" src={{ url('js/app.js') }} defer>
+	</script>
+</head>
+
+<body>
+	<main class="grid h-screen place-items-center">
+		<div class="flex flex-col items-center">
 			<a href="{{ url('/') }}">
-				<img src={{ url('img/logo.png') }} class="center"/>
+				<img  class="h-32 md:h-48 lg:h-64" src={{ url('img/logo.png') }}>
 			</a>
-            <section id="content" class="m-2">
-                @yield('content')
-            </section>
-        </main>
-		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-		<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-    </body>
+			<section id="content">
+				@yield('content')
+			</section>
+		</div>
+	</main>
+</body>
+
 </html>
