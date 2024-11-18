9e6989a86048ec9372652adf6afb4fa9a73c5ecc
Gabriel Lima <44784408+gablm@users.noreply.github.com>
2024-11-18 15:27:57 +0000

    login

--- src/resources/views/auth/login.blade.php
+++ src/resources/views/auth/login.blade.php
@@ -6,36 +6,36 @@
 		{{ csrf_field() }}
 
 		<div class="mb-4">
-			<label class="block text-gray-700 text-sm font-bold mb-2" for="email">E-mail</label>
-			<input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="email" name="email" value="{{ old('email') }}" required>
+			<label class="auth" for="email">E-mail</label>
+			<input class="auth focus:outline-none focus:shadow-outline" id="email" type="email" name="email" value="{{ old('email') }}" required>
 			@if ($errors->has('email'))
-			<span class="text-red-500 text-xs bold">
+			<span class="auth-error bold">
 				{{ $errors->first('email') }}
 			</span>
 			@endif
 		</div>
 
 		<div class="mb-4">
-			<label class="block text-gray-700 text-sm font-bold mb-2" for="password">Password</label>
-			<input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" name="password" required>
+			<label class="auth" for="password">Password</label>
+			<input class="auth focus:outline-none focus:shadow-outline" id="password" type="password" name="password" required>
 			@if ($errors->has('password'))
-			<span class="text-red-500 text-xs bold">
+			<span class="auth-error bold">
 				{{ $errors->first('password') }}
 			</span>
 			@endif
 		</div>
 
 		<div class="mb-6">
-			<label class="appearance-none rounded text-gray-700 leading-tight focus:outline-none">
+			<label>
 				<input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
 			</label>
 		</div>
 
 		<div class="flex items-center justify-between">
-			<button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+			<button class="auth-main focus:outline-none focus:shadow-outline" type="submit">
 				Sign In
 			</button>
-			<a class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="{{ route('register') }}">
+			<a class="auth" href="{{ route('register') }}">
 				Register
 			</a>
 		</div>
