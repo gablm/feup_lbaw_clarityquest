fed9323fb51dd7191d7d53b6d44fb83fc44c7155
Gabriel Lima <44784408+gablm@users.noreply.github.com>
2024-11-19 14:40:54 +0000

    Update footer.blade.php

--- src/resources/views/layouts/footer.blade.php
+++ src/resources/views/layouts/footer.blade.php
@@ -1,12 +1,12 @@
 <div>
   <hr>
-  <footer class="z-index-1 shadow-lg px-2">
-  	<div class="flex justify-between items-center p-2 text-xs">
+  <footer class="bg-gray-100 z-index-1 shadow-lg p-2 px-4">
+  	<div class="flex justify-between items-center text-xs">
         <p>Copyright &copy; ClarityQuest 2024</p>
 		<div class="flex justify-evenly space-x-4 items-center text-xs">
+			<a href="#" class="hover:underline">FAQ</a>	
 			<a href="#" class="hover:underline">About Us</a>
 			<a href="#" class="hover:underline">Contacts</a>
-			<a href="#" class="hover:underline">Frequently Asked Questions (FAQ)</a>
 		</div>
     </div>
   </footer>
