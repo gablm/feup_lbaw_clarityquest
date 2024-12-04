14d6052c500627147c551a484033b68359dbfe22
Sara Cortez <saracortez@Computador-portatil-de-Sara.local>
2024-12-04 11:53:44 +0000

    changed some styles

--- src/resources/views/partials/medals.blade.php
+++ src/resources/views/partials/medals.blade.php
@@ -1,25 +1,20 @@
 <div class="container mx-auto p-4">
-    <div class="profile-header">
-        <h1>Your Medals</h1>
-    </div>
-
     <div class="medals-section">
         @if ($medals)
             <article class="w-full bg-white shadow-md rounded-lg mb-4">
-                <div class="flex flex-col p-4">
-                    <div class="flex flex-row items-center text-gray-500 text-sm mb-1">
+                <div class="grid grid-cols-4 p-4">
+                    <div class="flex items-center justify-center col-span-1">
                         <img
                             src="{{ url('img/medal.svg') }}"
                             alt="Medal Icon"
-                            class="w-12 h-12 object-cover mr-4">
-                        <span class="text-lg font-semibold">Medals</span>
+                            class="w-32 h-auto object-cover">
                     </div>
-                    <div class="text-gray-700 mb-3 mt-1 break-words ml-16">
-                        <p>Posts Upvoted: {{ $medals->posts_upvoted }}</p>
-                        <p>Posts Created: {{ $medals->posts_created }}</p>
-                        <p>Questions Created: {{ $medals->questions_created }}</p>
-                        <p>Answers Posted: {{ $medals->answers_posted }}</p>
-                        <p>Total Medals: {{ $user->totalMedals() }}</p>
+                    <div class="col-span-2 text-gray-700 mb-3 mt-1 break-words">
+                        <p class="text-gray-700"><b> Posts Upvoted: </b>{{ $medals->posts_upvoted }}</p>
+                        <p class="text-gray-700"><b> Posts Created: </b>{{ $medals->posts_created }}</p>
+                        <p class="text-gray-700"><b> Questions Created: </b>{{ $medals->questions_created }}</p>
+                        <p class="text-gray-700"><b> Answers Posted: </b>{{ $medals->answers_posted }}</p>
+                        <p class="text-black"><b> Total: </b>{{ $user->totalMedals() }}</p>
                     </div>
                 </div>
             </article>
