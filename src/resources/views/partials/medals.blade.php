dfad51d5022a5aa7881d4d01559b50268ad0061e
Sara Cortez <saracortez@Computador-portatil-de-Sara.local>
2024-12-04 11:42:21 +0000

    changed some styles

--- src/resources/views/partials/medals.blade.php
+++ src/resources/views/partials/medals.blade.php
@@ -4,15 +4,27 @@
     </div>
 
     <div class="medals-section">
-        <h2>Medals</h2>
         @if ($medals)
-            <p>Posts Upvoted: {{ $medals->posts_upvoted }}</p>
-            <p>Posts Created: {{ $medals->posts_created }}</p>
-            <p>Questions Created: {{ $medals->questions_created }}</p>
-            <p>Answers Posted: {{ $medals->answers_posted }}</p>
-            <p>Total Medals: {{ $user->totalMedals() }}</p>
+            <article class="w-full bg-white shadow-md rounded-lg mb-4">
+                <div class="flex flex-col p-4">
+                    <div class="flex flex-row items-center text-gray-500 text-sm mb-1">
+                        <img
+                            src="{{ url('img/medal.svg') }}"
+                            alt="Medal Icon"
+                            class="w-12 h-12 object-cover mr-4">
+                        <span class="text-lg font-semibold">Medals</span>
+                    </div>
+                    <div class="text-gray-700 mb-3 mt-1 break-words ml-16">
+                        <p>Posts Upvoted: {{ $medals->posts_upvoted }}</p>
+                        <p>Posts Created: {{ $medals->posts_created }}</p>
+                        <p>Questions Created: {{ $medals->questions_created }}</p>
+                        <p>Answers Posted: {{ $medals->answers_posted }}</p>
+                        <p>Total Medals: {{ $user->totalMedals() }}</p>
+                    </div>
+                </div>
+            </article>
         @else
-            <p>No medals available.</p>
+            <p class="text-gray-700">No medals available.</p>
         @endif
     </div>
 </div>
