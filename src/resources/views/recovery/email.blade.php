477a78c20b2c888509ad8571ef5ecae56f66aa90
Gabriel Lima <44784408+gablm@users.noreply.github.com>
2024-12-10 23:32:43 +0000

    ui for email pt2

--- src/resources/views/recovery/email.blade.php
+++ src/resources/views/recovery/email.blade.php
@@ -1,13 +1,15 @@
 @extends('layouts.auth')
 
 @section('content')
-<div class="flex flex-col justify-center space-y-4">
-	<p>Hello, {{ $mailData['name'] }}!</p>
-	<p>A password recovery request for the account associated with this email ({{ $mailData['email'] }}) was requested.</p>
-	<p>Click the button below to proceed!</p>
-	<p>If this email was not requested by you, please ignore it.</p>
+<div class="flex flex-col justify-center space-y-4 max-md:max-w-[50vw] max-w-[30vw]">
+	<p>Hello, <span class="font-bold">{{ $mailData['name'] }}</span>!</p>
+	<p>A password recovery request for the account associated
+		with this email (<span class="font-bold">{{ $mailData['email'] }}</span>)
+		was requested.</p>
+	<p>Click the link below to continue.</p>
 	<a class="auth-link" href="{{ url('/recover/' . $mailData['token']) }}">
 		Recover password
 	</a>
+	<p class="font-bold">If this email was not requested by you, please ignore it.</p>
 </div>
 @endsection
