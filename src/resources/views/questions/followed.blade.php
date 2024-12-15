a8901cb6b9a5f43aac2f6d147f6ba8741cf091a7
Sara Cortez <saracortez@Computador-portatil-de-Sara.local>
2024-12-15 12:30:17 +0000

    updated sidebar

--- src/resources/views/questions/followed.blade.php
+++ src/resources/views/questions/followed.blade.php
@@ -7,6 +7,7 @@
         <h2 class="text-2xl font-semibold mb-4">
             Followed Questions
             @include('partials.tip', ['tip' => "Here are the questions you follow."])
+        <h2
         @if($followedQuestions->isEmpty())
             <p class="text-gray-700">You are not following any questions.</p>
         @else
